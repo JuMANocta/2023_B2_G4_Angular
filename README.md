03e18cabb5c6282c19f6341f465856f59c2868a6
dev <julien.mangeat@mail-formateur.net>
2023-02-03 11:27:34 +0100

    ajout de hero-details en composant ainsi qu'un service

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.2.
 
+```bash
+npm install -g @angular/cli # install angular en global (-g) dans nodeJS
+npm install #installer les dépendences du projet
+ng serve # lance le projet
+```
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-
 import { Hero } from '../hero';
-
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,5 +10,15 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent {
   //hero = 'Windstorm';
-  heroes = HEROES;
+  // heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+  heroes: Hero[] = [];
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+  
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
