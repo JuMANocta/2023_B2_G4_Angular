0e3c57ae44ec4b42c8e9abdbe5c864d8d4f4fd65
dev <julien.mangeat@mail-formateur.net>
2023-02-16 12:02:28 +0100

    ajout de message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 // import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +12,7 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent {
   //hero = 'Windstorm';
   // heroes = HEROES;
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
   heroes: Hero[] = [];
 
   ngOnInit(): void {
@@ -21,10 +22,20 @@ export class HeroesComponent {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    // this.heroService.getHeroes().subscribe(
+    //   (heroes) => {
+    //     this.heroes = heroes;
+    //   }
+    //une fonction fléchée est de nature
+    // =>
+    // ()=>{}
+    // function(){}
+    console.log('HeroesComponent.getHeroes()')
   }
   
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: l'id du héro est ${hero.id}`)
   }
 }
---
+++ src/app/message.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  constructor() { }
+
+  messages: string[] = [];
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
+
+  clearMessage(message: string){
+    // // index de mon message ?
+    // const index = this.messages.indexOf(message);
+    // // if si l'index est bien dans le tableau de messages
+    // if(index !== -1){
+    //   // si il est bien dedant alors splice de mon tableau
+    //   this.messages.splice(index, 1)
+    // }
+    this.messages = this.messages.filter(m => m!==message)
+  }
+}
